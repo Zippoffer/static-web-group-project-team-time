b5f664b58f71c624e5ddf7f438a95165fcc912ff
Banks Humphreys <iobg@Bankss-MacBook-Pro.local>
2016-05-25 12:01:07 -0500

    product page finished

--- products.js
+++ products.js
@@ -1,43 +1,43 @@
 var productArray = [
 	{
 	name: "Rope Bird House",
-	description: "This is a rope birdhouse",
-	price: "1.00",
+	description: "Luxury penthouse in the sky for wayward avians",
+	price: "Priceless",
 	url:  "birdfeed.jpg"},
 	{
-	name: "rope",
-	description: "rope",
-	price: "1.00",
+	name: "Kentucky Derby Hat",
+	description: "Fine twine chapeau",
+	price: "Contact for price",
 	url:  "hat.jpg"},
 	{
-	name: "rope",
-	description: "rope",
-	price: "1.00",
+	name: "Designer Fashion Mannequin",
+	description: "Hemphead",
+	price: "$15.00",
 	url:  "head.jpg"},
 	{
-	name: "rope",
-	description: "rope",
-	price: "1.00",
+	name: "Designer Shoes",
+	description: "The finest open toed fashion accessory",
+	price: "$34.99",
 	url:  "shoes.jpg"},
 	{
 	name: "rope",
-	description: "rope",
-	price: "1.00",
+	description: "what is",
+	price: "relative",
 	url:  "stringtheory.gif"},
 	{
-	name: "rope",
-	description: "rope",
-	price: "1.00",
+	name: "Fine Braded Digits",
+	description: "Custom, Hand-crafted, Made-to-order, Rope numerals to satisfy your every need",
+	price: "$350",
 	url:  "numbers.gif"},
 	{
-	name: "rope",
-	description: "rope",
-	price: "1.00",
+	name: "Pure Rope",
+	description: "The purest rope known to man. Discovered in the ruins Angkor Wat",
+	price: "៛57.31",
 	url:  "rope.jpg"},
 	{
-	name: "rope",
-	description: "rope",
-	price: "1.00",
+	name: "Rope + Coins",
+	description: "A circle of rope with money in it",
+	price: "$0.57",
 	url:  "coinrope.jpg"},];
 
 
@@ -47,7 +47,7 @@ for(i=0; i< productArray.length; i++){
 	var card=document.getElementById("product"+i);
 	console.log(card);
 	card.innerHTML = "<h2>" + productArray[i].name + "</h2>" + "<p>" + productArray[i].description + "</p>"
-	+ "<img src='" + productArray[i].url + "'" + "<p> The price is: " + productArray[i].price+ "</p>";
+	+ "<img src='" + productArray[i].url + "'" + "<p> Price: " + productArray[i].price+ "</p>";
 }
 
 
